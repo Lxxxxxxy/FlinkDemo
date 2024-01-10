0e5c2392f89a9b9f5346c35e88acf02798d64bf0
Lxxxxxxy <597973086@qq.com>
2024-01-10 10:02:55 +0800

    init

--- src/main/java/cn/lixingyu/UnboundedDataStreamDemo.java
+++ src/main/java/cn/lixingyu/UnboundedDataStreamDemo.java
@@ -1,7 +1,27 @@
 package cn.lixingyu;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
 /**
- * @author Lxxxxxxy_ 
+ * @author Lxxxxxxy_
  * @date 2024/01/03 10:19
  */
 public class UnboundedDataStreamDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> dataStreamSource = executionEnvironment.socketTextStream("localhost", 9000);
+        SingleOutputStreamOperator<Tuple2<String, Integer>> returns =
+            dataStreamSource.flatMap((String line, Collector<Tuple2<String, Integer>> out) -> {
+                for (String word : line.split(" ")) {
+                    out.collect(new Tuple2<String, Integer>(word, 1));
+                }
+            }).returns(Types.TUPLE(Types.STRING, Types.INT));
+        returns.keyBy(data -> data.f0).sum(1).print();
+        executionEnvironment.execute();
+    }
 }
